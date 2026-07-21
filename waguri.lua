71b77fd805009ee425d95d9745b642d29a418176
Waguriii2 <janishamoraleya@gmail.com>
2026-07-21 22:09:48 +0800

    Update and rename waguri.lua to wagurii.lua

--- waguri.lua
+++
@@ -1,2 +0,0 @@
-loadstring(game:HttpGet("https://cdn.sourceb.in/bins/gVUmVzKzMg/0", true))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Waguriiiii/Murder-mystery-2/refs/heads/main/Waguri.lua"))()
---
+++ wagurii.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("https://cdn.sourceb.in/bins/NWXGyWVOKP/0", true))()
